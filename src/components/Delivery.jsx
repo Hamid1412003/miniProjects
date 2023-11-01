aa5c25634313dec437c2b1c122f6124e597ea774
HAMID A <athakkakathhamid88@gmail.com>
2023-11-01 10:24:17 +0530

    i add files

---
+++ src/components/Delivery.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Delivery = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+        <h3 className='text-orange-500 font-bold text-2xl text-center' >Qick Delivery App</h3>
+        <div className='w-[1540] mx-auto grid md:grid-cols-2'>
+            <img className='w-[550px] mx-auto my-4' src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp' />
+            <div className='flex flex-col justify-center'>
+                <p className='text-[#00df9a] font-bold'>Get The App</p>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Limitless Convenience on-demand</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur assumenda, optio atque cupiditate deserunt tenetur mollitia placeat nostrum in, quo repudiandae commodi autem nam aliquid quia minus error fugit quasi.
+                    Doloribus esse consectetur odio quae accusamus deleniti delectus odit nemo eligendi, consequatur, nulla reiciendis nostrum iure facere harum recusandae atque vitae. Aut nemo quos quia quae quo, inventore soluta explicabo.
+                    Maxime officiis aut molestiae eum numquam! Commodi, magnam debitis incidunt adipisci vel ipsam sequi similique quam molestias, hic impedit beatae temporibus laboriosam eos pariatur ipsa! Sapiente impedit nisi quasi fugit!
+                    Tempora perferendis optio repellat, quidem illum earum error aperiam quae dolor fugiat molestias, doloribus perspiciatis vero provident eaque non, porro quaerat eveniet reprehenderit mollitia deserunt doloremque quibusdam! Alias, velit sapiente.
+                    Nostrum error, neque nulla vel odit maiores nemo quaerat iure blanditiis aspernatur eos officiis possimus tempore minus aliquid nam. Deserunt atque dolorum nesciunt eius cum architecto voluptas qui delectus excepturi.
+                </p>
+                <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'> Get Started</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Delivery
